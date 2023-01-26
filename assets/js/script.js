98e33ed06fe889c05c8d13bbbb07fcaf9718ec50
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-01-26 16:39:40 -0500

    Solved acrash issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,44 +22,88 @@
 // TODO: Add code to display the current date in the header of the page.
 // });
 
-let plans = {};
 
-function init() {
-  let savedPlans = JSON.parse(localStorage.getItem("plans"));
-  if (savedPlans !== null) {
-    plans = savedPlans;
-  }
-  renderPlans();
-  loadCurrentDateTime();
-}
 
-function loadCurrentDateTime(){
+// let plans = {};
+
+// function init() {
+//   let savedPlans = JSON.parse(localStorage.getItem("plans"));
+//   if (savedPlans !== null) {
+//     plans = savedPlans;
+//     renderPlans();
+//   } else {
+//     plans ={};
+//   }
+//   loadCurrentDateTime();
+// }
+
+// $(function loadCurrentDateTime(){
+//   $("#currentDay").text(dayjs().format("dddd")+", "+dayjs().format('MMMM D'));
+//   let currentHour = dayjs().format("H");
+//   for (let i = 9; i < 18; i++){
+//     if (i < currentHour){
+//       $(`#hour-${i}`).addClass("past");
+//     } else if (i = currentHour){
+//       $(`#hour-${i}`).addClass("present");
+//     } else {
+//       $(`#hour-${i}`).addClass("future");
+//     }
+//   }
+// })
+
+// function renderPlans(){
+//   $.each(Object.keys(plans), function(i, key){
+//     $(`#${key}`).children("textarea").text(plans[key]);
+//   })
+// }
+
+// $(".saveBtn").on("click", async function(event){
+//   await init();
+//   event.preventDefault;
+//   let plan = $(this).parent().children("textarea").val();
+//   let hourId = $(this).parent().attr("id");
+//   plans[hourId] = plan;
+//   localStorage.setItem("plans", JSON.stringify(plans));
+// });
+
+// init();
+
+// let plans = {"hour-9":"Meeting", "hour-11": "break"};
+
+$(function(){
+
+  let plans = {};
+
+  $(".saveBtn").on("click", function(event){
+    event.preventDefault;
+    let plan = $(this).parent().children("textarea").val();
+    let hourId = $(this).parent().attr("id");
+    plans[hourId] = plan;
+    localStorage.setItem("plans", JSON.stringify(plans));
+  });
+
   $("#currentDay").text(dayjs().format("dddd")+", "+dayjs().format('MMMM D'));
   let currentHour = dayjs().format("H");
   for (let i = 9; i < 18; i++){
     if (i < currentHour){
       $(`#hour-${i}`).addClass("past");
-    } else if (i = currentHour){
-      $(`#hour-${i}`).addClass("present");
-    } else {
+      console.log(i + "Past");
+    } else if (i > currentHour){
       $(`#hour-${i}`).addClass("future");
+      console.log(i + "Future");
+    } else {
+      $(`#hour-${i}`).addClass("present");
+      console.log(i + "Present");
     }
   }
-}
 
-function renderPlans(){
-  $.each(Object.keys(plans), function(i, key){
+  let savedPlans = JSON.parse(localStorage.getItem("plans"));
+    if (savedPlans !== null) {
+      plans = savedPlans;
+    }
+
+  $.each(Object.keys(plans), function(j, key){
     $(`#${key}`).children("textarea").text(plans[key]);
   })
-}
 
-$(".saveBtn").on("click", async function(event){
-  await init();
-  event.preventDefault;
-  let plan = $(this).parent().children("textarea").val();
-  let hourId = $(this).parent().attr("id");
-  plans[hourId] = plan;
-  localStorage.setItem("plans", JSON.stringify(plans));
 });
-
-init();
